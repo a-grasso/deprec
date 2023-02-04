64526105719edb85cf19bea2cef3a3c6330d3c9e
a-grasso <antonino.grasso.it@gmail.com>
2023-02-04 11:47:37 +0100

    feat: add agent result data sources

--- agent/agent.go
+++ agent/agent.go
@@ -15,6 +15,17 @@ type Result struct {
 	Dependency      model.Dependency
 	Core            model.CoreResult
 	Recommendations model.RecommendationDistribution
+	DataSources     []string
+}
+
+func (ar *Result) RecommendationsInsights() string {
+
+	dm := ar.Recommendations[model.DecisionMaking]
+	w := ar.Recommendations[model.Watchlist]
+	nia := ar.Recommendations[model.NoImmediateAction]
+	nc := ar.Recommendations[model.NoConcerns]
+
+	return fmt.Sprintf("DM: %.3f | W: %.3f | NIA: %.3f | NC: %.3f", dm, w, nia, nc)
 }
 
 func (ar *Result) ToString() string {
@@ -59,7 +70,7 @@ func NewAgent(dependency model.Dependency, configuration configuration.Configura
 }
 
 func (agent *Agent) Run() Result {
-	agent.Extraction()
+	dataSources := agent.Extraction()
 
 	result := agent.CombinationAndConclusion()
 
@@ -67,24 +78,31 @@ func (agent *Agent) Run() Result {
 		Dependency:      agent.Dependency,
 		Core:            result,
 		Recommendations: result.Softmax(),
+		DataSources:     dataSources,
 	}
 }
 
-func (agent *Agent) Extraction() {
+func (agent *Agent) Extraction() []string {
 
+	var dataSources []string
 	cache := cache.NewCache(agent.Config.MongoDB)
 
 	if vcs, exists := agent.Dependency.ExternalReferences[model.VCS]; exists && strings.Contains(vcs, "github") {
 		extraction.NewGitHubExtractor(agent.Dependency, agent.Config.GitHub, cache).Extract(agent.DataModel)
+		dataSources = append(dataSources, "github")
 	}
 
 	if agent.Dependency.PackageURL != "" {
 		extraction.NewOSSIndexExtractor(agent.Dependency, agent.Config.OSSIndex, cache).Extract(agent.DataModel)
+		dataSources = append(dataSources, "ossindex")
 	}
 
 	if sha1, exists := agent.Dependency.Hashes[model.SHA1]; exists && sha1 != "" {
 		extraction.NewMavenCentralExtractor(agent.Dependency, cache).Extract(agent.DataModel)
+		dataSources = append(dataSources, "mavencentral")
 	}
+
+	return dataSources
 }
 
 func (agent *Agent) CombinationAndConclusion() model.CoreResult {
@@ -109,7 +127,7 @@ func (agent *Agent) CombinationAndConclusion() model.CoreResult {
 	circumstances := cores.Circumstances(agent.DataModel)
 
 	cr.Overtake(deityGiven, 100)
-	cr.Overtake(vulnerabilities, 25)
+	cr.Overtake(vulnerabilities, 5)
 	cr.Overtake(effort, 1)
 	cr.Overtake(support, 1)
 	cr.Overtake(circumstances, 1)
